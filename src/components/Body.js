2fe042d4c05c53ab27df771c9c86f3538f998159
Neha Nadakuditi <nehanadakuditi@gmail.com>
2025-06-07 01:50:34 -0400

    optional chaining and useEffect hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const Body=() => {
-    const [listOfRestaurants,setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants , setListOfRestaurants] = useState([]);
+    useEffect(()=>{
+       fetchData();
+    },
+    
+    [])
+     
     const handleFilter=()=>{
       console.log("Button clicked!");
       const filteredList = listOfRestaurants.filter(
          (res) => parseFloat(res.info.avgRating) > 4
-      
       );
       setListOfRestaurants(filteredList);
+    }
+    const fetchData= async() =>{
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.5036955&lng=80.6290471&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING#");
+      const json= await data.json;
+      setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants[1]?.info);
+      //setListOfRestaurants(resList);
+        
     }
 
     return (
